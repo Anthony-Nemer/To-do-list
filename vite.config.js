e71dc8ded98ef7faa058ec12f07c20df607a5afb
Anthony-nemer <anthonynemer000@gmail.com>
2024-11-21 01:42:50 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/To-do-list/', // For your custom domain
+  base: '/', // For your custom domain
 });
