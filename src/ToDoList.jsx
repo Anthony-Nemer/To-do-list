77443d5ea1ce4e00f5879b2c2384725ea97f9ad8
Anthony-nemer <anthonynemer000@gmail.com>
2024-11-22 02:04:18 +0200

    Update ToDoList.jsx

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -48,7 +48,7 @@ function ToDoList() {
 
     return (
         <div className='to-do-list'>
-            <h1>To-Do-List</h1>
+            <h1>To-Do-List BLUH BLUG BLUHH</h1>
             <form onSubmit={addTask}>
                 <input
                     type='text'
